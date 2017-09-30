a366af8977a58637fd0a972fc1b875282e495bc4
Philip Cavaco <philipcavaco@gmail.com>
2017-09-30 23:16:12 +0100

    Keep arg keys and display name if present.

--- httpfunction/run.py
+++ httpfunction/run.py
@@ -1,11 +1,12 @@
 import os
 import json
 
-get_args = None
+get_arg_prefix = "REQ_QUERY_"
+get_args = {}
 
 for var in os.environ:
-    if var.startswith("REQ_QUERY_"):
-        get_args = os.environ[var]
+    if var.startswith(get_arg_prefix):
+        get_args[var[len(get_arg_prefix):].lower()] = os.environ[var]
 
 print "Get Args", get_args
 
@@ -15,7 +16,7 @@ returnData = {
     "status": 200,
     
     #Response Body:
-    "body": "<h1>Azure Works :)</h1>",
+    "body": "<h1>Azure Works :) {}</h1>".format(get_args.get("name", ""),
     
     # Send any number of HTTP headers
     "headers": {
