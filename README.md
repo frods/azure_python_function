22da4ef7a0f4a2d67eb9175d720bfc8265e2d127
Philip Cavaco <philipcavaco@gmail.com>
2017-09-30 17:20:14 +0100

    dummy httpfunction

---
+++ README.md
@@ -0,0 +1,4 @@
+Some Azure functions to understand how it works.
+
+Based on [documentation](https://docs.microsoft.com/en-us/azure/azure-functions/functions-reference#folder-structure) from Microsoft.
+
---
+++ httpfunction/run.py
@@ -0,0 +1,18 @@
+print "Test"
+
+returnData = {
+    #HTTP Status Code:
+    "status": 200,
+    
+    #Response Body:
+    "body": "<h1>Azure Works :)</h1>",
+    
+    # Send any number of HTTP headers
+    "headers": {
+        "Content-Type": "text/html",
+        "X-Awesome-Header": "YesItIs"
+    }
+}
+
+output = open(os.environ['res'], 'w')
+output.write(json.dumps(returnData))
